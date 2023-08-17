cc08f431a9a3ebf7b906cf8bfbdf02e282798c90
Saishree Taluri <139688449+SAISHREE-T@users.noreply.github.com>
2023-08-17 16:58:36 +0530

    Update and rename dictionary.py to Emoji_converter.py
    Convert characters to emojis!

--- Emoji_converter.py
+++ Emoji_converter.py
@@ -1,26 +1,3 @@
-customer={
-    'Name': "saishree",
-    "age": 16,
-    "is_verified":True
-}
-customer["Name"]="Mika"
-customer["phno."]=9123678450
-print(customer.get("Name"))
-print(customer["Name"])
-print(customer)
-
-#EXERCISE
-number=(input("Phone: "))
-digits={
-    "1":"one",
-    "2":"two",
-    "3":"three",
-    "4":"four"
-    }
-output=""
-for x in number:
-    output+=digits.get(x,"!")+" "
-    print(output)
 
 #Emoji converter
 statement=input("> ")
